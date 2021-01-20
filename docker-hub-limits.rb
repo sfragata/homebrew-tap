39aa1014fd9643f3717f50f05a210f43a17366a4
sfragata <sfragata@gmail.com>
2021-01-19 23:23:20 -0500

    Brew formula update for docker-hub-limits version v1.6

--- docker-hub-limits.rb
+++ docker-hub-limits.rb
@@ -5,20 +5,20 @@
 class DockerHubLimits < Formula
   desc "Utility to check docker download rate limits""
   homepage "https://github.com/sfragata/docker-hub-limits/"
-  version "1.3"
+  version "1.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sfragata/docker-hub-limits/releases/download/v1.3/docker-hub-limits_1.3_Darwin_x86_64.tar.gz"
-    sha256 "6309cc0b04cc4bfa103a83070da6ccb3ffea6da462ab807f29c394585ec90892"
+    url "https://github.com/sfragata/docker-hub-limits/releases/download/v1.6/docker-hub-limits_1.6_Darwin_x86_64.tar.gz"
+    sha256 "22c79f1c9c45ee84a8ea184f721f0714a29a2d5bbebab5e9c56625a68a3f73c8"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/sfragata/docker-hub-limits/releases/download/v1.3/docker-hub-limits_1.3_Linux_x86_64.tar.gz"
-    sha256 "cf894490a0c5dbcd970fb04f11140f232603fbb14d040e996e621bc012d27c87"
+    url "https://github.com/sfragata/docker-hub-limits/releases/download/v1.6/docker-hub-limits_1.6_Linux_x86_64.tar.gz"
+    sha256 "3cf0babc2260c6bce816ca3f9ee66a07ee9e0824ca7bf428705858f772f1b256"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/sfragata/docker-hub-limits/releases/download/v1.3/docker-hub-limits_1.3_Linux_arm64.tar.gz"
-    sha256 "14e769fdcb12dd5381246180bd7ec73e51331b7f260af64480c4f1c873eb7fb4"
+    url "https://github.com/sfragata/docker-hub-limits/releases/download/v1.6/docker-hub-limits_1.6_Linux_arm64.tar.gz"
+    sha256 "0d2afb363b3186fae9982b31cafcb6dc7cb45af45bb6a05ec81999fe6c55e1f4"
   end
 
   def install
